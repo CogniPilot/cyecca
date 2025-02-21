7d2cb9f061f4c3e992fc67d36020443bcc3f5879
wsribunm <worawissribunma@gmail.com>
2025-02-21 14:20:23 -0500

    clean up code
    Signed-off-by: wsribunma <worawissribunma@gmail.com>

--- cyecca/models/fixedwing.py
+++ cyecca/models/fixedwing.py
@@ -5,53 +5,49 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from cyecca.lie.group_so3 import SO3Quat, SO3EulerB321
 import cyecca.lie as lie
-# from tf_transformations import euler_from_quaternion
-
 
 def derive_model():
     # p, parameters
-    thr_max = ca.SX.sym("thr_max")
-    m = ca.SX.sym("m")
-    # cl = ca.SX.sym("cl")
-    cd = ca.SX.sym("cd")
-    S = ca.SX.sym("S")
-    rho = ca.SX.sym("rho")
-    g = ca.SX.sym("g")
-    Jx = ca.SX.sym("Jx")
-    Jy = ca.SX.sym("Jy")
-    Jz = ca.SX.sym("Jz")
-    J = ca.diag(ca.vertcat(Jx, Jy, Jz))
+    thr_max = ca.SX.sym("thr_max") #maximum thrust
+    m = ca.SX.sym("m") # mass
+    cd = ca.SX.sym("cd") # drag coefficient
+    S = ca.SX.sym("S") # Wing Surface area
+    rho = ca.SX.sym("rho") # Air Density
+    g = ca.SX.sym("g") #Gravitational Acceleration (m/s^2)
+    Jx = ca.SX.sym("Jx") # Moment of Inertia in x direction
+    Jy = ca.SX.sym("Jy") # Moment of Inertia in y direction
+    Jz = ca.SX.sym("Jz") # Moment of Inertia in z direction
+    J = ca.diag(ca.vertcat(Jx, Jy, Jz)) # Moment of Inertia Array
     Cm_p = ca.SX.sym("Cm_p") #moment coefficient for roll
     Cm_q = ca.SX.sym("Cm_q") #moment coefficient for pitch
     Cm_r = ca.SX.sym("Cm_r") #moment coefficent for yaw
     cbar = ca.SX.sym("cbar") #mean chord (m)
     span = ca.SX.sym("span") #wing span (m)
     cla = ca.SX.sym("cla") # Cl_alpha per alpha
-    cl0 = ca.SX.sym("cl0")
+    cl0 = ca.SX.sym("cl0") # Coefficient of lift at zero alpha
     p= ca.vertcat(
-        thr_max,
-        m,
+        thr_max, 
+        m,  
         # cl,
-        cd,
-        S,
-        rho,
-        g,
+        cd, 
+        S, 
+        rho, 
+        g, 
         Jx,
         Jy,
-        Jz,
-        Cm_p,
-        Cm_q,
-        Cm_r,
-        cbar,
-        span,
-        cla,
-        cl0
+        Jz, 
+        Cm_p, 
+        Cm_q, 
+        Cm_r, 
+        cbar, 
+        span, 
+        cla, 
+        cl0  
     )
     p_defaults = {
-        "thr_max" : 4.50,
+        "thr_max" : 4.50, 
         "m" : 0.5,
-        # "cl": 6.28,
-        "cd" : 0.3, #0.1,
+        "cd" : 0.3,
         "S": 1.0,
         "rho": 1.225,
         "g": 9.8,
@@ -74,9 +70,9 @@ def derive_model():
     omega_wb_b = ca.SX.sym("omega_wb_b",3) # world-body
 
     x = ca.vertcat(
-        position_w,
-        velocity_b,
-        quat_wb,
+        position_w, # position in world frame
+        velocity_b, # velocity in body frame
+        quat_wb, 
         omega_wb_b,
         )
     x0_defaults = {
@@ -108,8 +104,6 @@ def derive_model():
     zAxis = ca.vertcat(0, 0, 1)
 
     V_b = ca.norm_2(velocity_b)
-    # frac_alp = ca.if_else(ca.fabs(velocity_b[2]/velocity_b[0])> 0.1,  (velocity_b[2]/velocity_b[0]), ca.SX(0))
-    # alpha = ca.if_else(ca.fabs(velocity_b[0]) > 1e-4, ca.atan(frac_alp), ca.SX(0))
 
     alpha = ca.if_else(ca.fabs(velocity_b[0]) > 1e-3, ca.atan(velocity_b[2]/velocity_b[0]), ca.SX(0))
     vxvb = ca.if_else(ca.fabs(velocity_b[1]/V_b) < 1, (velocity_b[1]/V_b), ca.sign(velocity_b[1]/V_b))
@@ -129,9 +123,8 @@ def derive_model():
     # Force and Moment Model
     cl = cl0 + cla*(-1*alpha) # Lift Coefficient
     
-    # qbar = 0.5 * rho * velocity_b[0]**2  # qbar in terms of body vel_x
-    qbar = 0.5 * rho * ca.norm_2(velocity_w_w)**2 # TODO Recheck wind frame velocity --> qbar in terms of wind velocity
-
+    qbar = 0.5 * rho * ca.norm_2(velocity_w_w)**2 
+    
     ground = ca.if_else(position_w[2]< 0.0,
         -position_w[2] * 1500 *zAxis - velocity_b * 1,
         ca.vertcat(0,0,0))
@@ -144,7 +137,7 @@ def derive_model():
     F_b = ca.vertcat(0,0,0)
     F_n = ca.vertcat(-D, Fs, L) #force in wind frame (n)
     F_b += quat_bn @ ca.SX(F_n) # Aerodynamic force from wind in body frame
-    F_b += quat_bw @ ground # TODO current method does not consider wheel friction on ground
+    F_b += quat_bw @ ground
     F_b += quat_bw @ (-m * g * zAxis) # gravity converted to body frame
 
     throttle = ca.if_else(u[0] > 1e-6, u[0], 1e-6)
@@ -154,20 +147,12 @@ def derive_model():
     # Moment
     M_b = ca.vertcat(0, 0, 0)
 
-    # Direct Command Rotation
-    # M_b += (u[1]-omega_wb_b[0]) * Cm_p *xAxis #moment due to roll angle command
-    # M_b += (u[2]-omega_wb_b[1]) * 0.1 *yAxis #moment due to pitch angle command
-    # M_b += (u[3]-omega_wb_b[2]) * 0.1 *zAxis #moment due to yaw angle command
-
     # Rotation by aerodynamic moment
     M_b += Cm_p * qbar * S * span *(u[1]) * xAxis- 0.1*omega_wb_b*xAxis#roll moment due to aileron
     M_b += Cm_q * qbar * S * cbar * (u[2]) * yAxis - 0.1*omega_wb_b*yAxis #pitch moment due to elevator deflection
     M_b += Cm_r * qbar * S * span *(u[3]) * zAxis - 0.05*omega_wb_b*zAxis #yaw moment due to rudder
     Cm_yr = 0.01 # Moment coefficent for yaw due to aileron rolling
     M_b += Cm_yr * qbar * S * span *(-u[1]-omega_wb_b[2]) * zAxis #yaw moment due to aileron
-    # Cm_thr = 0.001 # Moment coefficient for pitch due to throttle
-    # M_b += -1 *(Cm_thr * u[0] - omega_wb_b[0]) * yAxis # moment due to throttle
-
 
     ##############################################################################################
     
@@ -188,7 +173,6 @@ def derive_model():
     # algebraic (these algebraic expressions are used during the simulation)
     z = ca.vertcat()
     alg = z
-
 
     f = ca.Function("f", [x, u, p], [xdot], ["x", "u", "p"], ["xdot"])
 

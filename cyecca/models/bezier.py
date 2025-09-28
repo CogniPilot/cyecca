0caf260a3eab76140a2d34126ce018897f177351
Li-Yu Lin <liyu8561501@gmail.com>
2025-09-28 11:04:29 -0400

    reformat
    Signed-off-by: Li-Yu Lin <liyu8561501@gmail.com>

--- cyecca/models/bezier.py
+++ cyecca/models/bezier.py
@@ -20,34 +20,37 @@ J_xz = 0
 
 
 class Bezier:
-#https://en.wikipedia.org/wiki/B%C3%A9zier_curve
+    # https://en.wikipedia.org/wiki/B%C3%A9zier_curve
 
     def __init__(self, P: ca.SX, T: float):
         self.P = P
         self.m = P.shape[0]
-        self.n = P.shape[1]-1
+        self.n = P.shape[1] - 1
         self.T = T
-    
+
     def eval(self, t):
-        #https://en.wikipedia.org/wiki/De_Casteljau%27s_algorithm
-        beta = t/self.T
+        # https://en.wikipedia.org/wiki/De_Casteljau%27s_algorithm
+        beta = t / self.T
         A = ca.SX(self.P)
         for j in range(1, self.n + 1):
             for k in range(self.n + 1 - j):
                 A[:, k] = A[:, k] * (1 - beta) + A[:, k + 1] * beta
         return A[:, 0]
-    
+
     def deriv(self, m=1):
         D = ca.SX(self.P)
         for j in range(0, m):
-            D = (self.n - j)*ca.horzcat(*[ D[:, i+1] - D[:, i] for i in range(self.n - j) ])
-        return Bezier(D/self.T**m, self.T)
+            D = (self.n - j) * ca.horzcat(
+                *[D[:, i + 1] - D[:, i] for i in range(self.n - j)]
+            )
+        return Bezier(D / self.T**m, self.T)
+
 
 def derive_bezier7():
     n = 8
-    T = ca.SX.sym('T')
-    t = ca.SX.sym('t')
-    P = ca.SX.sym('P', 1, n)
+    T = ca.SX.sym("T")
+    t = ca.SX.sym("t")
+    P = ca.SX.sym("P", 1, n)
     B = Bezier(P, T)
 
     # derivatives
@@ -67,8 +70,8 @@ def derive_bezier7():
     r = ca.vertcat(p, v, a, j, s)
 
     # given position/velocity boundary conditions, solve for bezier points
-    wp_0 = ca.SX.sym('p0', 4, 1)  # pos/vel at waypoint 0
-    wp_1 = ca.SX.sym('p1', 4, 1)  # pos/vel at waypoint 1
+    wp_0 = ca.SX.sym("p0", 4, 1)  # pos/vel at waypoint 0
+    wp_1 = ca.SX.sym("p1", 4, 1)  # pos/vel at waypoint 1
 
     constraints = []
     constraints += [(B.eval(0), wp_0[0])]  # pos @ wp0
@@ -79,25 +82,30 @@ def derive_bezier7():
     constraints += [(B_d.eval(T), wp_1[1])]  # vel @ wp1
     constraints += [(B_d2.eval(T), wp_1[2])]  # zero accel @ wp1
     constraints += [(B_d3.eval(T), wp_1[3])]  # zero accel @ wp1
-    
+
     assert len(constraints) == n
 
     Y = ca.vertcat(*[c[0] for c in constraints])
     b = ca.vertcat(*[c[1] for c in constraints])
     A = ca.jacobian(Y, P)
     A_inv = ca.inv(A)
-    P_sol = (A_inv@b).T
+    P_sol = (A_inv @ b).T
 
     return {
-        'bezier7_solve': ca.Function('bezier7_solve', [wp_0, wp_1, T], [P_sol], ['wp_0', 'wp_1', 'T'], ['P']),
-        'bezier7_traj': ca.Function('bezier7_traj', [t, T, P], [r], ['t', 'T', 'P'], ['r']),
+        "bezier7_solve": ca.Function(
+            "bezier7_solve", [wp_0, wp_1, T], [P_sol], ["wp_0", "wp_1", "T"], ["P"]
+        ),
+        "bezier7_traj": ca.Function(
+            "bezier7_traj", [t, T, P], [r], ["t", "T", "P"], ["r"]
+        ),
     }
+
 
 def derive_bezier3():
     n = 4
-    T = ca.SX.sym('T')
-    t = ca.SX.sym('t')
-    P = ca.SX.sym('P', 1, n)
+    T = ca.SX.sym("T")
+    t = ca.SX.sym("t")
+    P = ca.SX.sym("P", 1, n)
     B = Bezier(P, T)
 
     # derivatives
@@ -115,8 +123,8 @@ def derive_bezier3():
     r = ca.vertcat(p, v, a)
 
     # given position/velocity boundary conditions, solve for bezier points
-    wp_0 = ca.SX.sym('p0', 2, 1)  # pos/vel at waypoint 0
-    wp_1 = ca.SX.sym('p1', 2, 1)  # pos/vel at waypoint 1
+    wp_0 = ca.SX.sym("p0", 2, 1)  # pos/vel at waypoint 0
+    wp_1 = ca.SX.sym("p1", 2, 1)  # pos/vel at waypoint 1
 
     constraints = []
     constraints += [(B.eval(0), wp_0[0])]  # pos @ wp0
@@ -130,16 +138,16 @@ def derive_bezier3():
     b = ca.vertcat(*[c[1] for c in constraints])
     A = ca.jacobian(Y, P)
     A_inv = ca.inv(A)
-    P_sol = (A_inv@b).T
+    P_sol = (A_inv @ b).T
 
     functions = [
-        ca.Function('bezier3_solve', [wp_0, wp_1, T], [P_sol],
-            ['wp_0', 'wp_1', 'T'], ['P']),
-        ca.Function('bezier3_traj', [t, T, P], [r],
-            ['t', 'T', 'P'], ['r']),
+        ca.Function(
+            "bezier3_solve", [wp_0, wp_1, T], [P_sol], ["wp_0", "wp_1", "T"], ["P"]
+        ),
+        ca.Function("bezier3_traj", [t, T, P], [r], ["t", "T", "P"], ["r"]),
     ]
 
-    return { f.name(): f for f in functions }
+    return {f.name(): f for f in functions}
 
 
 def derive_dcm_to_quat():

ae3db56c98ae537cc68fc470ad1df9f55a419122
James Goppert <james.goppert@gmail.com>
2025-11-25 12:36:28 -0500

    Make pydot import optional in graph.py to fix CI

--- cyecca/graph.py
+++ cyecca/graph.py
@@ -5,12 +5,31 @@ import tempfile
 import casadi as ca
 from casadi import print_operator as print_operator_orig
 import casadi.tools.graph as mod_graph
-import pydot
+
+try:
+    import pydot
+    PYDOT_AVAILABLE = True
+except ImportError:
+    PYDOT_AVAILABLE = False
+    pydot = None
 
 
 def draw_casadi(
     expr: ca.SX or ca.MX, filename: str = None, width: int = 1920, direction: str = "LR"
 ) -> pydot.Graph:
+    """
+    Draw a CasADi expression graph.
+    
+    Requires pydot to be installed: pip install pydot
+    or install cyecca with visualization extras: pip install cyecca[visualization]
+    """
+    if not PYDOT_AVAILABLE:
+        raise ImportError(
+            "pydot is required for graph visualization. "
+            "Install it with: pip install pydot "
+            "or: pip install cyecca[visualization]"
+        )
+    
     curdir = Path(os.path.abspath(os.getcwd()))
     # dot draw creates a source.dot file, lets move to the tmp directory
     os.chdir(tempfile.gettempdir())
